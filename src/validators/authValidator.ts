4f61c80a7a80214d08f457aab57f3685b5683d4e
Ossamanadifi <ossamanadifi@gmail.com>
2026-06-30 13:50:28 +0200

    personalizzazione errori

--- src/validators/authValidator.ts
+++ src/validators/authValidator.ts
@@ -2,15 +2,15 @@ import { z } from "zod";
 
 //definizione dello schema zod dei dati in input per la registrazione
 const registerSchema = z.object({
-    name : z.string().trim().min(2, "Il nome deve essere almeno di 2 caratteri"),
+    name : z.string("Inserisci un nome valido").trim().min(2, "Il nome deve essere almeno di 2 caratteri"),
     email : z.email("Inserisci un' email valida").toLowerCase(),
-    pswd : z.string().min(6, "La password deve essere almeno di 6 caratteri"),
+    pswd : z.string("Inserisci una password valida").min(6, "La password deve essere almeno di 6 caratteri"),
 });
 
 //definizione dello schema zod dei dati in input per il login
 const loginSchema = z.object({
     email : z.email("Inserisci un' email valida").toLowerCase(),
-    pswd : z.string().min(1, "Inserisci una password valida"),
+    pswd : z.string("Inserisci una password valida").min(6),
 });
 
 export { registerSchema, loginSchema };
--- src/validators/taskValidator.ts
+++ src/validators/taskValidator.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 //definizione dello schema zod dei dati in input per la creazione di una task
 const taskSchema = z.object({
-    title : z.string().trim().min(2, "inserisci un titolo valido"),
-    description : z.string().trim().min(2,"Inserisci una descrizione valida"),
+    title : z.string("inserisci un titolo valido").trim().min(2, "inserisci un titolo valido"),
+    description : z.string("inserisci una descrizione valida").trim().min(2,"Inserisci una descrizione valida"),
     state : z.enum(["PLANNED", "COMPLETED"], {
         error: () => ({
             message: "Status must be one of: PLANNED, COMPLETED",
